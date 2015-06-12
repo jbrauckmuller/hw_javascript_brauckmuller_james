98e1a29aed47f2f6c678e1001ead112e8a71b0a9
Jamie Brauckmuller <jamiebrauckmuller@gmail.com>
2015-06-12 00:01:45 -0400

    presidents commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+var presidents = [
+	{
+		firstName: 'Barack',
+		lastName: 'Obama',
+		termLength: '6 years',
+		party: 'Democrat',
+		yearsOfPresidency: '2009-2015'
+	},
+
+	{
+		firstName: 'George',
+		lastName: 'Bush',
+		termLength: '8 years',
+		party: 'Republican',
+		yearsOfPresidency: '2001-2009'
+	}
+
+	{
+		firstName: 'Bill',
+		lastName: 'Clinton',
+		termLength: '8 years',
+		party: 'Democrat',
+		yearsOfPresidency: '1993-2001'
+	},
+
+	{
+		firstName: 'George H W',
+		lastName: 'Bush',
+		termLength: '4 years',
+		party: 'Republican',
+		yearsOfPresidency: '1989-1993'
+	},
+
+	{
+		firstName: 'Ronald',
+		lastName: 'Reagan',
+		termLength: '8 years',
+		party: 'Republican',
+		yearsOfPresidency: '2001-2009'
+	}
+];
+	
+console.log(presidents[2].firstName + " " + presidents[2].lastName);
